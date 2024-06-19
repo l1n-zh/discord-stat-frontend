8b78f3893f5bed7419a096a833e20942f0c6e0d8
L1n <89698082+l1n-zh@users.noreply.github.com>
2024-06-19 19:54:13 +0800

    fix: 12:00 should be "12PM"

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,7 @@ function generate24HourArray() {
 
     for (let i = 0; i < 24; i++) {
         const hour = {
-            title: i <= 12 ? `${i} AM` : `${i - 12} PM`,
+            title: i < 12 ? `${i} AM` : `${i > 12 ? i-12 : 12} PM`,
             subtitle: `${i < 9 ? "0" : ""}${i}:00 ~ ${i < 9 ? "0" : ""}${i}:59`,
             value: i,
         };
